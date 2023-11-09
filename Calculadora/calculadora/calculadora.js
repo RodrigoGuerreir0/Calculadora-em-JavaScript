3d0793b11e247d5a9a680def9a176658ec82e5f4
Rodrigo Guerreiro <rodrigoguerreiro217@gmail.com>
2023-11-09 11:39:43 -0300

    Add files via upload

---
+++ Calculadora/calculadora/calculadora.js
@@ -0,0 +1,16 @@
+function calcular(operador){
+    numero1 = parseFloat(document.getElementById('numero1').value)
+    numero2 = parseFloat(document.getElementById('numero2').value)
+
+    if(operador == "+"){
+        resultado= numero1+numero2
+    }else if(operador == "-"){
+        resultado= numero1-numero2
+    }else if(operador == "/"){
+        resultado= numero1/numero2
+    }else if(operador == "*"){
+        resultado= numero1*numero2
+    }
+
+    document.getElementById('resultado').innerHTML = resultado
+}
